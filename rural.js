537024d6f15a005f2fb28911b9428a571c3037cf
Taylor Downs <downs.taylor@gmail.com>
2020-02-07 14:23:13 +0000

    finished round of WCS change requests

--- rural.js
+++ rural.js
@@ -96,18 +96,60 @@ sql(state => {
 sql(state => {
   const { data } = state;
   return (
-    `insert into "tbl_wildmeat" (
-      "unit",
-      "amount",
-      "massin_grams",
-      "price",
-      "acquisition",
-      "condition",
-      "currency"
-  ) values ('` +
+    `insert into "tbl_sample" ("` +
+    [
+      // 'sampleID',
+      'sampleDateStart',
+      'sampleDateEnd',
+      // 'householdID',
+      // 'householdCharID',
+      'sampleUnit',
+      'numberSampleUnits',
+      'samplingEffortInDays',
+      'siteID',
+      'studyID',
+    ].join('", "') +
+    `") values ('` +
+    [
+      // data._uuid,
+      data['survey_info/info_recall_date'],
+      data['survey_info/info_recall_date'],
+      // data._uuid,
+      // data._uuid,
+      data['group_begin/group_sample/sample_unit'],
+      data['group_begin/group_sample/number_sample_units'],
+      data['group_begin/group_sample/sampling_effort_in_days'],
+      data.__query_params.siteId,
+      data.__query_params.studyId,
+    ]
+      .join("', '")
+      .replace(/''/g, null) +
+    `');`
+  );
+});
+
+sql(state => {
+  const { data } = state;
+  return (
+    `insert into "tbl_wildmeat" ("` +
+    [
+      'siteID',
+      'sampleID',
+      'unit',
+      'amount',
+      'massinGrams',
+      'price',
+      'acquisition',
+      'condition',
+      'currency',
+      'scientificName',
+    ].join('", "') +
+    `") values ('` +
     data['group_begin/group_food']
       .map(i =>
         [
+          data.__query_params.siteId,
+          data._uuid,
           i['group_begin/group_food/quantity_technique'] === 'known_quantity'
             ? 'kilogram'
             : 'other',
@@ -122,6 +164,7 @@ sql(state => {
             ? '-8'
             : i['group_begin/group_food/state'],
           'CDF',
+          i['group_begin/group_food/species'],
         ].join("', '")
       )
       .join("'), ('")
@@ -130,29 +173,28 @@ sql(state => {
   );
 });
 
-sql(state => {
-  const { data } = state;
-  return (
-    `insert into "tbl_sample" (
-      "sample_date_start",
-      "sample_date_end",
-      "household_id",
-      "household_char_id",
-      "sample_unit",
-      "number_sample_units",
-      "sampling_effort_in_days"
-  ) values ('` +
-    [
-      data['survey_info/info_recall_date'],
-      data['survey_info/info_recall_date'],
-      '', // household_id
-      '', // household_char_id
-      data['group_begin/group_sample/sample_unit'],
-      data['group_begin/group_sample/number_sample_units'],
-      data['group_begin/group_sample/sampling_effort_in_days'],
-    ]
-      .join("', '")
-      .replace(/''/g, null) +
-    `');`
-  );
-});
+// NOTE: We are not currently loading individuals.
+// sql(state => {
+//   const { data } = state;
+//   return (
+//     `insert into "tbl_individual" ("` +
+//     [
+//       'gender',
+//       'age',
+//       'born_in_village',
+//       'ethnicity',
+//       'years_of_education',
+//     ].join('", "') +
+//     `") values ('` +
+//     [
+//       data.gender,
+//       data.age,
+//       data.born_in_village,
+//       data.ethnicity,
+//       data.years_of_education,
+//     ]
+//       .join("', '")
+//       .replace(/''/g, null) +
+//     `');`
+//   );
+// });
