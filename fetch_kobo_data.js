afbb1c4631d7cf75d992efe9df7a82e397eb6ea7
mamadou <mamadou.lakhassane@gmail.com>
2020-08-25 18:46:30 +0000

    new jobs for mapping Kobo form to Wildmeat DB, #3

---
+++ fetch_kobo_data.js
@@ -0,0 +1,75 @@
+//== Job to be used for fetching data from Kobo on repeated, timer basis  ==//
+// This can be run on-demand at any time by clicking "run" //
+
+alterState(state => {
+  // Set a manual cursor if you'd like to only fetch data after this date.
+  manualCursor = '2020-05-25T14:32:43.325+01:00';
+  state.data = {
+    surveys: [
+      { id: 'afCPRfan8JdBt9sy9q48Jp', tag: 'Rural Consumption' }, //Form Id, Tag of test OpenFN BNS Survey form --> For Testing
+      // Add more Form ids and tags to this list to perform more Kobo syncs...
+      //{ id: 'atyo55YdBdfxzXiaBdrbvr', tag: 'bns_2019' }, //BNS Price survey to be synced regularly
+      //{ id: 'aTRKQW2b8TJGxF7DVPfjFv', tag: 'bns_price_2019' }, //BNS Price survey to be synced regularly
+    ].map(survey => ({
+      formId: survey.id,
+      tag: survey.tag,
+      url: `https://kf.kobotoolbox.org/api/v2/assets/${survey.id}/data/?format=json`,
+      //query: `&query={"end":{"$gte":"${state.lastEnd || manualCursor}"}}`,
+    })),
+  };
+  return state;
+});
+
+alterState(state => {
+    console.log(state);
+
+    return state;
+})
+
+each(dataPath('surveys[*]'), state =>
+  //get(`${state.data.url}${state.data.query}`, {}, state => {
+  get(`${state.data.url}`, {}, state => {
+    state.data.submissions = state.data.results.map(submission => ({
+      //Here we append the tags defined above to the Kobo form submission data
+      form: lastReferenceValue('tag')(state),
+      body: submission,
+    }));
+    console.log(`Fetched ${state.data.count} submissions.`);
+    //Once we fetch the data, we want to post each individual Kobo survey
+    //back to the OpenFn inbox to run through the jobs
+    return each(
+      dataPath('submissions[*]'),
+      post(
+        state.configuration.openfnInboxUrl,
+        { body: state => state.data },
+        state => {
+          const delay = 1000;
+          console.log(`Waiting ${delay}ms. ⏱️`);
+          function timer() {
+            return new Promise(resolve => {
+              setTimeout(() => {
+                resolve('Done. 👍');
+              }, delay);
+            });
+          }
+
+          async function msg() {
+            const msg = await timer();
+            console.log(msg);
+            return state;
+          }
+
+          return msg();
+        }
+      )
+    )(state);
+  })(state)
+);
+
+alterState(state => {
+  const lastEnd = state.references
+    .filter(item => item.body)
+    .map(s => s.body.end)
+    .sort((a, b) => new Date(b.date) - new Date(a.date))[0];
+  return { ...state, lastEnd };
+});
---
+++ ruralConsumptionToPostgres.js
@@ -0,0 +1,94 @@
+upsert('tbl_study', 'study_id', {
+  study_id: 1000,
+});
+
+upsert('tbl_site', 'site_id', {
+  site_id: 1001,
+  admin_level_3: state.data['survey_info/district'],
+  site_name: state.data['survey_info/village'],
+});
+
+upsert('tbl_household', 'external_id', {
+  study_id: 1000,
+  site_id: 1001,
+  household_id: state.data['survey_info/household_id'],
+  external_id: state.data['survey_info/household_id'],
+});
+
+upsert('tbl_household_char', '', {
+  site_id: 1001,
+  study_id: 1000,
+  household_id: state.data['survey_info/household_id'],
+  num_occupants: state.data['group_begin/group_people/nb_people'],
+  num_babies: state.data['group_begin/group_people/nb_babies'],
+  num_children: state.data['group_begin/group_people/nb_children'],
+  num_young_men: state.data['group_begin/group_people/nb_youngmen'],
+  num_adult_men: state.data['group_begin/group_people/nb_men'],
+  num_old_men: state.data['group_begin/group_people/nb_oldmen'],
+  num_adult_women: state.data['group_begin/group_people/nb_women'],
+  num_pregnant_women: state.data['group_begin/group_people/nb_pregnant'],
+  num_breastfeeding_women:
+    state.data['group_begin/group_people/nb_brestfeeding'],
+});
+
+upsert('tbl_sample', 'sample_id', {
+  study_id: 1000,
+  site_id: 1001,
+  household_id: state.data['survey_info/household_id'],
+  household_char_id: state.data['survey_info/household_id'],
+  date_start: state.data['survey_info/info_recall_date'],
+  sample_id:
+    state.data._id + state.data._submission_time + state.data._xform_id_string,
+  sample_unit: 'kilograms',
+  number_sample_units: '24',
+  sampling_effortin_days: '2',
+});
+
+insertMany('tbl_wildmeat', state =>
+  state.data['group_begin/group_food'].map(foodItem => {
+    return {
+      site_id: 1001,
+      study_id: 1000,
+      sample_id:
+        state.data._id +
+        state.data._submission_time +
+        state.data._xform_id_string,
+      vernacular_name: foodItem['group_begin/group_food/species'],
+      wildmeat_category_1: foodItem['group_begin/group_food/category1'],
+      wildmeat_category_2: foodItem['group_begin/group_food/category2'],
+      wildmeat_group: foodItem['group_begin/group_food/group'],
+      unit:
+        foodItem['group_begin/group_food/quantity_technique'] ===
+        'known_technique'
+          ? 'kilogram'
+          : '-8',
+      amount: foodItem['group_begin/group_food/amount'],
+      massin_grams: foodItem['group_begin/group_food/quantity'],
+      price: foodItem['group_begin/group_food/Cost'],
+      aquisition: foodItem['group_begin/group_food/obtention'],
+      acquisition_other: foodItem['group_begin/group_food/other_obtention'],
+      origin_of_wildmeat: foodItem['group_begin/group_food/origin_wildmeat'],
+      condition: foodItem['group_begin/group_food/state'],
+      consumption_frequency_unit: foodItem['group_begin/group_food/frequency'],
+    };
+  })
+);
+
+upsert('tbl_individual', '', {
+  site_id: 1001,
+  study_id: 1000,
+  household_id: state.data['survey_info/household_id'],
+  external_id: state.data['survey_info/identity'],
+});
+
+upsert('swm_transaction', 'uuid', {
+  uuid:
+    state.data._id + state.data._submission_time + state.data._xform_id_string,
+  date: Date.now(),
+  status: 'new',
+  submission_time: state.data._submission_time,
+  modified_by: 'open_fn',
+  inserted_by: 'open_fn',
+  data_type: 'consumption',
+  instances: state.data,
+});
