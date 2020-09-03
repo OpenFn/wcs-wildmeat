ec53cb82350e9d48a8a207bf0e692cd9c4319af7
Taylor Downs <downs.taylor@gmail.com>
2020-09-03 09:08:54 -0400

    debug callback for kobo cursor

--- fetch_kobo_data.js
+++ fetch_kobo_data.js
@@ -57,9 +57,14 @@ each(dataPath('forms[*]'), state =>
 );
 
 alterState(state => {
+  console.log(state);
+  console.log(state.references);
   const lastEnd = state.references
     .filter(item => item.body)
-    .map(s => s.body.end)
+    .map(s => {
+      console.log(s);
+      return s.body.end;
+    })
     .sort((a, b) => new Date(b.date) - new Date(a.date))[0];
   return { ...state, lastEnd };
 });
