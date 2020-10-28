d68f06f54ba70d6a812c5db7ddebc68cfc271cdc
Taylor Downs <taylor@openfn.org>
2020-10-28 06:41:12 +0000

    taylor@openfn.org's change on OpenFn.org

--- jobs/fetch-rc-submissions.js
+++ jobs/fetch-rc-submissions.js
@@ -1,5 +1,5 @@
-//== Job to be used for fetching data from Kobo on repeated, timer basis  ==//
-// This can be run on-demand at any time by clicking "run" //
+// Job to be used for fetching data from Kobo on repeated, timer basis
+// This can be run on-demand at any time by clicking "run"
 console.log('Inspecting initial state...')
 console.log(JSON.stringify({ ...state, configuration: 'REDACTED' }, null, 2));
 
