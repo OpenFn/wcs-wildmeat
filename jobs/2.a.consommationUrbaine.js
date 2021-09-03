1b840560d684ae19680751f8257225c4370187d6
mamadou <mamadou@openfn.org>
2021-09-03 15:58:16 +0000

    map urban consumption, #26

--- jobs/2.a.consommationUrbaine.js
+++ jobs/2.a.consommationUrbaine.js
@@ -1,19 +1,39 @@
 fn(state => {
+  const studyIDMap = {
+    'Urban Consumption': 1000,
+    'Rural Consumption': 1002,
+    Market: 1004,
+    Offtake: 1006,
+  };
+
   return combine(
     upsert('tbl_study', 'study_id', {
-      study_id: 1000,
+      study_id: studyIDMap[state.data.formType],
     }),
 
     // to check
-    upsert('tbl_wildmeat_urban', 'study_id', {
-      study_id: 1000,
-      sample_id: `${state.data._id}${state.data._xform_id_string}`,
-      unit: '',
-      vernacular_name: '',
+    fn(state => {
+      const wildmeatIDs =
+        state.data.body['domeat_consumption/domeat_species'].split(' ');
+      return upsertMany('tbl_wildmeat_urban', 'study_id', state => {
+        wildmeatIDs.map(wildmeat => {
+          return {
+            study_id: studyIDMap[state.data.formType],
+            wildmeat_id: wildmeat,
+            sample_id: `${state.data._id}${state.data._xform_id_string}`,
+            amount: state.data.body[`domeat_consumption/quantity_${wildmeat}`],
+            condition: state.data.body[`domeat_consumption/state_${wildmeat}`],
+            wildmeat_group:
+              state.data.body['domeat_consumption/domeat_species'],
+            unit: state.data.body['domeat_consumption/qty_measure_type_dm'],
+            vernacular_name: state.data.body['bm_consumption/bm_species'],
+          };
+        });
+      });
     }),
 
     upsert('tbl_site', 'study_id', {
-      study_id: 1000,
+      study_id: studyIDMap[state.data.formType],
       admin_level_2: state.data.body['introduction_gp/other_town'],
     }),
 
@@ -21,21 +41,21 @@ fn(state => {
       sample_id: `${state.data._id}${state.data._xform_id_string}`,
       date_start: state.data.body['introduction_gp/date'],
       date_end: state.data.body['introduction_gp/date'],
-      preferences: '',
+      preferences: state.data.body['prot_preference/why_like_bm'],
     }),
 
-    upsert('swm_species', 'study_id', {
-      study_id: 1000,
-    }),
+    // upsert('swm_species', 'study_id', {
+    // taxonID: '',
+    // vernacularName: '',
+    // }),
 
     upsert('tbl_individual_urban', 'study_id', {
-      study_id: 1000,
       gender: state.data.body['introduction_gp/gender'],
       local_origin: state.data.body['subject_info/home'],
-      origin_type: '',
+      origin_type: state.data.body['subject_info/other_home'],
       age: state.data.body['subject_info/age'],
       education_years: state.data.body['subject_info/education_yrs'],
       religion: state.data.body['other_questions/religion'],
     })
-  );
+  )(state);
 });
