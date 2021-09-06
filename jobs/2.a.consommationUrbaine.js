01692e205cb9f65e03fe109aa5b16c84e086e2f7
mamadou <mamadou@openfn.org>
2021-09-06 16:33:21 +0000

    remove combine

--- jobs/2.a.consommationUrbaine.js
+++ jobs/2.a.consommationUrbaine.js
@@ -13,28 +13,24 @@ upsert('tbl_study', 'study_id', {
   study_id: state => state.studyIDMap[state.data.formType],
 });
 
-upsert('tbl_study', 'study_id', {
-  study_id: state => state.studyIDMap[state.data.formType],
-});
-
-fn(state => {
-  const wildmeatIDs =
-    state.data.body['domeat_consumption/domeat_species'].split(' ');
-  return upsertMany('tbl_wildmeat_urban', 'wildmeat_id', state =>
-    wildmeatIDs.map(wildmeat => {
-      return {
-        study_id: state => state.studyIDMap[state.data.formType],
-        wildmeat_id: wildmeat,
-        sample_id: `${state.data._id}${state.data._xform_id_string}`,
-        amount: state.data.body[`domeat_consumption/quantity_${wildmeat}`],
-        condition: state.data.body[`domeat_consumption/state_${wildmeat}`],
-        wildmeat_group: state.data.body['domeat_consumption/domeat_species'],
-        unit: state.data.body['domeat_consumption/qty_measure_type_dm'],
-        vernacular_name: state.data.body['bm_consumption/bm_species'],
-      };
-    })
-  )(state);
-});
+// fn(state => {
+//   const wildmeatIDs =
+//     state.data.body['domeat_consumption/domeat_species'].split(' ');
+//   return upsertMany('tbl_wildmeat_urban', 'wildmeat_id', state =>
+//     wildmeatIDs.map(wildmeat => {
+//       return {
+//         study_id: state => state.studyIDMap[state.data.formType],
+//         wildmeat_id: wildmeat,
+//         sample_id: `${state.data._id}${state.data._xform_id_string}`,
+//         amount: state.data.body[`domeat_consumption/quantity_${wildmeat}`],
+//         condition: state.data.body[`domeat_consumption/state_${wildmeat}`],
+//         wildmeat_group: state.data.body['domeat_consumption/domeat_species'],
+//         unit: state.data.body['domeat_consumption/qty_measure_type_dm'],
+//         vernacular_name: state.data.body['bm_consumption/bm_species'],
+//       };
+//     })
+//   )(state);
+// });
 
 upsert('tbl_site', 'site_id', {
   study_id: state => state.studyIDMap[state.data.formType],
--- jobs/2.c.market.js
+++ jobs/2.c.market.js
@@ -1,64 +1,62 @@
 // Your job goes here.
 fn(state => {
-  const studyIDMap = {
+  state.studyIDMap = {
     'Urban Consumption': 1000,
     'Rural Consumption': 1002,
     Market: 1004,
     Offtake: 1006,
   };
+  return state;
+});
 
-  return combine(
-    upsert('tbl_study', 'study_id', {
-      study_id: studyIDMap[state.data.formType],
-    }),
+upsert('tbl_study', 'study_id', {
+  study_id: state => studyIDMap[state.data.formType],
+});
 
-    upsert('tbl_sample_market', 'sample_id', {
-      sample_id: `${state.data._id}${state.data._xform_id_string}`,
-      date_start: state.data.body.today,
-      date_end: state.data.body.today,
-    }),
+upsert('tbl_sample_market', 'sample_id', {
+  sample_id: `${state.data._id}${state.data._xform_id_string}`,
+  date_start: state.data.body.today,
+  date_end: state.data.body.today,
+});
 
-    // upsert('swm_species', 'study_id', {
-    // taxonID: '',
-    // vernacularName: '',
-    // }),
+// upsert('swm_species', 'study_id', {
+// taxonID: '',
+// vernacularName: '',
+// }),
 
-    upsert('tbl_site', 'site_id', {
-      admin_level_3: state.data.body.district,
-      site_name: state.data.body.village,
-      site_id: studyIDMap[state.data.formType],
-    }),
+upsert('tbl_site', 'site_id', {
+  admin_level_3: state.data.body.district,
+  site_name: state.data.body.village,
+  site_id: state => studyIDMap[state.data.formType],
+});
 
- 
-    upsert('tbl_market', 'external_id', {
-      external_id: state.data.body.market,
-      number_tables_surveyed: state.data.body.total_surveyed,
-    }),
+upsert('tbl_market', 'external_id', {
+  external_id: state.data.body.market,
+  number_tables_surveyed: state.data.body.total_surveyed,
+});
 
-    fn(state => {
-      const vendors = state.data.body['vendor'];
+fn(state => {
+  const vendors = state.data.body['vendor'];
 
-      return each(vendors, state => {
-        const sales = state.data['vendor/sales'];
-        return upsertMany('tbl_wildmeat_market', 'wildmeat_id', state => {
-          sales.map(sale => {
-            return {
-              wildmeat_id: sale['vendor/sales/species'],
-              amount: 1,
-              wildmeat_category_1: sale['vendor/sales/category1'],
-              wildmeat_category_2: sale['vendor/sales/category2'],
-              wildmeat_group: sale['vendor/sales/group'],
-              vernacular_name: sale['vendor/sales/species'],
-              vernacular_name_other: sale['vendor/sales/othe_species'],
-              condition: sale['vendor/sales/condition'],
-              unit: sale['vendor/sales/unit'],
-              unit_other: sale['vendor/sales/other_unit'],
-              harvest_method: sale['vendor/sales/technique'],
-              price: sale['vendor/sales/price'],
-            };
-          });
-        })(state);
-      })(state);
-    })
-  )(state);
+  return each(vendors, state => {
+    const sales = state.data['vendor/sales'];
+    return upsertMany('tbl_wildmeat_market', 'wildmeat_id', state =>
+      sales.map(sale => {
+        return {
+          wildmeat_id: sale['vendor/sales/species'],
+          amount: 1,
+          wildmeat_category_1: sale['vendor/sales/category1'],
+          wildmeat_category_2: sale['vendor/sales/category2'],
+          wildmeat_group: sale['vendor/sales/group'],
+          vernacular_name: sale['vendor/sales/species'],
+          vernacular_name_other: sale['vendor/sales/othe_species'],
+          condition: sale['vendor/sales/condition'],
+          unit: sale['vendor/sales/unit'],
+          unit_other: sale['vendor/sales/other_unit'],
+          harvest_method: sale['vendor/sales/technique'],
+          price: sale['vendor/sales/price'],
+        };
+      })
+    )(state);
+  })(state);
 });
