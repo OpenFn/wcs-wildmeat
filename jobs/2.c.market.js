709fb132c6a3d71b9f493db0a2d50779c4080c44
mamadou <mamadou@openfn.org>
2021-09-08 17:24:20 +0000

    check if vendors or sales exist before upserting

--- jobs/2.c.market.js
+++ jobs/2.c.market.js
@@ -45,31 +45,39 @@ upsert('tbl_site', 'ON CONSTRAINT tbl_site_pkey', {
 fn(state => {
   const vendors = state.data.body['vendor'];
 
-  return each(vendors, state => {
-    const sales = state.data['vendor/sales'];
-    return upsertMany(
-      'tbl_wildmeat_market',
-      'ON CONSTRAINT tbl_wildmeat_market_pkey',
-      state =>
-        sales.map(sale => {
-          return {
-            sample_id: `${state.data._id}${state.data._xform_id_string}${sale['vendor/sales/othe_species']}${sale['vendor/sales/quantity']}${sale['vendor/sales/price']}`,
-            study_id: state.studyIDMap[state.formType], //ad
-            site_id: state.studyIDMap[state.formType], //ad
-            wildmeat_id: sale['vendor/sales/species'],
-            amount: 1,
-            wildmeat_category_1: sale['vendor/sales/category1'],
-            wildmeat_category_2: sale['vendor/sales/category2'],
-            wildmeat_group: sale['vendor/sales/group'],
-            vernacular_name: sale['vendor/sales/species'],
-            vernacular_name_other: sale['vendor/sales/othe_species'],
-            condition: sale['vendor/sales/condition'],
-            unit: sale['vendor/sales/unit'],
-            other_unit: sale['vendor/sales/other_unit'],
-            harvest_method: sale['vendor/sales/technique'],
-            price: sale['vendor/sales/price'],
-          };
-        })
-    )(state);
-  })(state);
+  if (vendors) {
+    return each(vendors, state => {
+      const sales = state.data['vendor/sales'];
+      if (sales) {
+        return upsertMany(
+          'tbl_wildmeat_market',
+          'ON CONSTRAINT tbl_wildmeat_market_pkey',
+          state =>
+            sales.map(sale => {
+              return {
+                sample_id: `${state.data._id}${state.data._xform_id_string}${sale['vendor/sales/othe_species']}${sale['vendor/sales/quantity']}${sale['vendor/sales/price']}`,
+                study_id: state.studyIDMap[state.formType], //ad
+                site_id: state.studyIDMap[state.formType], //ad
+                wildmeat_id: sale['vendor/sales/species'],
+                amount: 1,
+                wildmeat_category_1: sale['vendor/sales/category1'],
+                wildmeat_category_2: sale['vendor/sales/category2'],
+                wildmeat_group: sale['vendor/sales/group'],
+                vernacular_name: sale['vendor/sales/species'],
+                vernacular_name_other: sale['vendor/sales/othe_species'],
+                condition: sale['vendor/sales/condition'],
+                unit: sale['vendor/sales/unit'],
+                other_unit: sale['vendor/sales/other_unit'],
+                harvest_method: sale['vendor/sales/technique'],
+                price: sale['vendor/sales/price'],
+              };
+            })
+        )(state);
+      }
+      console.log('No sales array. Ignoring upsert...');
+      return state;
+    })(state);
+  }
+  console.log('No vendors array. Ignoring upsert...');
+  return state;
 });
